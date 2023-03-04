03df7dabc9929ac8009b14b156366597e2d004e6
Tiisetso Dinoko <dinokovonteese@gmail.com>
2023-03-04 22:33:43 +0200

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -7,12 +7,12 @@ import 'firebase/compat/auth';
 // import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: "AIzaSyCeeuN7zNmQ34G_yg6b0dAAiyor65HifpI",
-  authDomain: "razum-3a1e6.firebaseapp.com",
-  projectId: "razum-3a1e6",
-  storageBucket: "razum-3a1e6.appspot.com",
-  messagingSenderId: "219703900822",
-  appId: "1:219703900822:web:b0dee87769cf4d049520b7"
+   apiKey: "AIzaSyCwWNBBjF_nLF5ZvWqc8Iyg1vlJvYcGPT4",
+  authDomain: "razum-110bc.firebaseapp.com",
+  projectId: "razum-110bc",
+  storageBucket: "razum-110bc.appspot.com",
+  messagingSenderId: "625048968840",
+  appId: "1:625048968840:web:12fa950e50eec6d2de093d"
   };
 
 const firebase = Firebase.initializeApp(config);
